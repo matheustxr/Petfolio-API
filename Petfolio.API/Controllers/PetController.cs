445b6126b3ad63fdaffb81a1aa45c5c2f816ba27
Matheus Teixeira <matheustxr.dev@gmail.com>
2024-10-30 08:20:14 -0300

    create response error

--- Petfolio.API/Controllers/PetController.cs
+++ Petfolio.API/Controllers/PetController.cs
@@ -3,6 +3,7 @@ using Petfolio.Application.UseCases.Pet.Register;
 using Petfolio.Application.UseCases.Pet.Update;
 using Petfolio.Communication.Reponses;
 using Petfolio.Communication.Requests;
+using Petfolio.Communication.Responses;
 
 namespace Petfolio.API.Controllers;
 [Route("api/[controller]")]
@@ -11,6 +12,7 @@ public class PetController : ControllerBase
 {
     [HttpPost]
     [ProducesResponseType(typeof(ResponseRegisterPetJson), StatusCodes.Status201Created)]
+    [ProducesResponseType(typeof(ResponseErrorJson), StatusCodes.Status400BadRequest)]
     public IActionResult Register([FromBody] RequestPetJson request)
     {
         var response = new RegisterPetUseCase().Execute(request);
@@ -21,6 +23,7 @@ public class PetController : ControllerBase
     [HttpPut]
     [Route("{id}")]
     [ProducesResponseType(StatusCodes.Status204NoContent)]
+    [ProducesResponseType(typeof(ResponseErrorJson), StatusCodes.Status400BadRequest)]
     public IActionResult Update([FromRoute] int id, [FromBody] RequestPetJson request)
     {
         var useCase = new UpdatePetUseCase();
